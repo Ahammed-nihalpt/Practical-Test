f670302353aec37d68d722090c07d12109aa5926
Ahammed nihal pt <ahammednihalpt@gmail.com>
2023-03-22 11:02:17 +0530

    nav bar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="nav">
+      <div className="options">
+        <button>Home</button>
+        <button>list</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
